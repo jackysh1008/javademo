08ef08c29500bc7209ed7270d38bf6ffda8f28cc
jackysh1008 <jackysh1008@163.com>
2014-10-11 11:35:10 +0800

    Create Demo.java
    this is a java demo

---
+++ Demo.java
@@ -0,0 +1,12 @@
+public class Demo {
+	
+	public static void doTest(){
+		System.out.println("do test ok");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("[doTest]");
+		doTest();
+	}
+
+}
